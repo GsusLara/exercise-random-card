a0a9fbf570213776c14257e4cbd29d3154e27945
Jesus Lara <jelara99@gmail.com>
2021-02-28 04:25:41 +0000

    ejercicio completado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./style.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let simbolo = selectorsimbolico();
+  const pintura = simbolo === "♥" || simbolo === "♦" ? "red" : "black";
+  this.document.querySelector(".numero").innerHTML = selectorNumeral();
+  this.document.querySelector(".numero").style.color = pintura;
+  this.document.querySelector(".icono1").innerHTML = simbolo;
+  this.document.querySelector(".icono1").style.color = pintura;
+  this.document.querySelector(".icono2").innerHTML = simbolo;
+  this.document.querySelector(".icono2").style.color = pintura;
+};
+let selectorNumeral = () => {
+  let numero = Math.floor(Math.random() * 14) + 1;
+  let salida = "";
+  if (numero > 10) {
+    switch (numero) {
+      case 11:
+        salida = "J";
+        break;
+      case 12:
+        salida = "Q";
+        break;
+      case 13:
+        salida = "K";
+        break;
+      case 14:
+        salida = "A";
+        break;
+      default:
+        salida = numero;
+    }
+  } else {
+    salida = numero;
+  }
+  return salida;
+};
+let selectorsimbolico = () => {
+  let numero = Math.floor(Math.random() * 4);
+  let salida = ["♥", "♦", "♠", "♣"];
+  return salida[numero];
 };
